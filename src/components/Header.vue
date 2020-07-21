c6afda787f62c270030030e94589cb291b29127b
Deepak <erdkgarg08@gmail.com>
2020-07-21 21:54:30 +0200

    project finished...just ui remaining and small bugs

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -12,13 +12,13 @@
                 <li class="nav-item">
                 <li class="nav-link" @click="endDay">End Day</li>
 
-                <li class="nav-item dropdown">
-                    <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">Save & Load</a>
-                    <div class="dropdown-menu">
-                        <a href="#" class="dropdown-item">Save Data</a>
-                        <a href="#" class="dropdown-item">Load Data</a>
-                    </div>
-                </li>
+<!--                <li class="nav-item dropdown">-->
+<!--                    <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">Save & Load</a>-->
+<!--                    <div class="dropdown-menu">-->
+<!--                        <a href="#" class="dropdown-item">Save Data</a>-->
+<!--                        <a href="#" class="dropdown-item">Load Data</a>-->
+<!--                    </div>-->
+<!--                </li>-->
                 <li class="nav-item">
                     <strong class="nav-link" >Funds : {{funds | currency}}</strong>
                 </li>
--- src/main.js
+++ src/main.js
@@ -1,12 +1,13 @@
 import Vue from "vue";
 import App from "./App.vue";
 import store from "./store/store";
-
+import vueResource from 'vue-resource'
 import VueRouter from 'vue-router'
 import {routes} from './routes'
 Vue.config.productionTip = false;
 
 Vue.use(VueRouter)
+Vue.use(vueResource)
 
 Vue.filter('currency', value => {
   return '$' + value.toLocaleString()
